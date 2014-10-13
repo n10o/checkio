f253b795ca21695ee9dbd905d95a963e369cf09c
n10o <0x0082@gmail.com>
2014-10-13 18:27:13 +0900

    Secret

---
+++ library2.0/secretmessage.py
@@ -0,0 +1,12 @@
+def find_message(text):
+  msg = ""
+  for txt in text:
+    if txt.isupper():
+      msg += txt[0]
+  return msg
+
+if __name__ == '__main__':
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert find_message(u"How are you? Eh, ok. Low or Lower? Ohhh.") == "HELLO", "hello"
+    assert find_message(u"hello world!") == "", "Nothing"
+    assert find_message(u"HELLO WORLD!!!") == "HELLOWORLD", "Capitals"
